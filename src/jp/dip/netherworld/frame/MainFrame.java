31b0db86be66a9a37b7296df25a3951482371005
takefumi.ota <takefumi.ota@mail.rakuten.com>
2013-04-10 19:18:18 +0900

    change for file choosing.

--- src/jp/dip/netherworld/frame/MainFrame.java
+++ src/jp/dip/netherworld/frame/MainFrame.java
@@ -4,12 +4,18 @@ import java.awt.AWTException;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.HeadlessException;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import jp.dip.netherworld.main.ScreenShot;
 
@@ -20,13 +26,16 @@ public class MainFrame extends JFrame {
 	private static final long serialVersionUID = -4419550151693231401L;
 
 	private ScreenShot logic;
+	private MainFrame self;
 
 	public MainFrame() throws HeadlessException {
 		super("スクリーンショットメーカー");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.self = this;
 
 		this.logic = new ScreenShot();
 
+		this.setMenuBar();
 		this.setContents(this.getContentPane());
 		this.setSize(300, 100);
 		this.setVisible(true);
@@ -47,13 +56,46 @@ public class MainFrame extends JFrame {
 				try {
 					logic.shot();
 				} catch (AWTException e) {
+					JOptionPane.showMessageDialog(self, "画像を出力できませんでした。\n"
+							+ "ディレクトリ： " + logic.getOutputDir() + "\n"
+							+ "が存在することを確認してください。");
 					e.printStackTrace();
 				} catch (IOException e) {
+					JOptionPane.showMessageDialog(self, "画像を出力できませんでした。\n"
+							+ "ディレクトリ： " + logic.getOutputDir() + "\n"
+							+ "が存在することを確認してください。");
 					e.printStackTrace();
 				}
 			}
 		});
 
 		container.add(ssButton);
+	}
+
+	private void setMenuBar() {
+		MenuBar mb = new MenuBar();
+		// メニューの生成
+		Menu mn = new Menu("ファイル");
+		mb.add(mn);
+		// メニュー項目の生成
+		MenuItem mi = new MenuItem("出力先の変更");
+		mi.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				fileChooser.setMultiSelectionEnabled(false);
+				int selected = fileChooser.showOpenDialog(self);
+				if (selected == JFileChooser.APPROVE_OPTION) {
+					File file = fileChooser.getSelectedFile();
+					if (file.isDirectory()) {
+						logic.setOutputDir(file.getAbsolutePath());
+					}
+				}
+			}
+		});
+
+		mn.add(mi);
+		this.setMenuBar(mb);
 	}
 }
